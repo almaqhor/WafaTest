1084438c97bc02dc5e784f817dfb404bb6501553
almaqhor <76499391+almaqhor@users.noreply.github.com>
2026-03-27 17:47:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const xlsx = require('xlsx');
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.post('/api/login', async (req, res) => {
+app.post('//auth/v1/login', async (req, res) => {
   try {
     const { username, password } = req.body;
     
